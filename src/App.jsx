b3ed3f642231fba633d25e85e169db6dc9875fdb
Yunus Parvez Khan <developer.ypk@gmail.com>
2025-01-29 01:12:12 +0530

    feat(router): create routing system

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
 import './App.css'
+import useNavigation from './hooks/useNavigation'
+import Home from './pages/Home';
+import Login from './pages/Login'
+import Router from './routers'
 
 function App() {
 
+  const { navigate } = useNavigation();
+
   return (
     <>
-      <h1>The Boring Tim</h1>
+
+      {/* Boilerplate Nav */}
+      {/* <div className='nav'>
+        <button onClick={() => navigate('home')}>Home</button>
+        <button onClick={() => navigate('login')}>Login</button>
+      </div> */}
+
+      {/* Routes --------------------------------------------- */}
+      <Router path="home" pathNum={1}>
+        <Home />
+      </Router>
+      <Router path="" pathNum={1}>
+        <Login />
+      </Router>
+      <Router path="login" pathNum={1}>
+        <Login />
+      </Router>
     </>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>The Boring Tim</div>
+    )
+}
+
+export default Home
